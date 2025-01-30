f4cd138f1f61ce9ca69ba9cac1dd270ad668ec8c
ovhibarua <hey.itsmeovhi@gmail.com>
2025-01-30 23:24:26 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-4xl font-extrabold'>hello world</h1>
+      <h1 className='text-4xl font-extrabold'>hello world</h1>
+      <h1 className='text-4xl font-extrabold'>hello world</h1>
+      <h1 className='text-4xl font-extrabold'>hello world</h1>
+    </>
+  )
+}
+
+export default App
